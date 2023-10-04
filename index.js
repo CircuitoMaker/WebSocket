3b9d36bfa1176314790604f5c860b0ea24d9c5b6
FillipeW <welausen@gmail.com>
2023-10-04 18:56:45 -0300

    update

--- index.js
+++ index.js
@@ -92,7 +92,7 @@ if (data.to === data.id) {
   } else {
     if (targetClient) {
     //targetClient.send(JSON.stringify({ message: `${data.message}` }));
-    targetClient.send(JSON.stringify(`${data.message}`));
+    targetClient.send(JSON.stringify(data.message));
   }}}
      
 //
